3943ebe7c2f28805952f3027cca62c9b6a9bdf4e
Leigh-Anne Niehaus <leigh-anneniehaus@Leigh-Annes-MacBook-Pro-2.local>
2023-06-30 10:34:02 -0700

    first commit

--- Part 1/app.js
+++ Part 1/app.js
@@ -2,23 +2,32 @@ let favNumber = 5;
 let baseURL = "http://numbersapi.com";
 
 // 1.
-$.getJSON(`${baseURL}/${favNumber}?json`).then(data => {
+async function part1() {
+  let data = await $.getJSON(`${baseURL}/${favNumber}?json`);
   console.log(data);
-});
+
+
+  console.log(data);
+};
+part1();
 //get json method returns a promise which is why .then is used to handle the response. 
 
 // 2.
-let favNumbers = [7, 11, 22];
-$.getJSON(`${baseURL}/${favNumbers}?json`).then(data => {
+const favNumbers = [7, 11, 22];
+async function part2() {
+  let data = await $.getJSON(`${baseURL}/${favNumbers}?json`);
   console.log(data);
-});
+};
+part2();
 
 // 3.
 //makes 4 requests to the Numbers API and prints a fact about your favorite number each time. 
-Promise.all(
-  Array.from({ length: 4 }, () => {
-    return $.getJSON(`${baseURL}/${favNumber}?json`);
-  })
-).then(facts => {
-  facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
-});
+async function part3() {
+  let facts = await Promise.all(
+    Array.from({ length: 4 }, () => $.getJSON(`${baseURL}/${favNumber}?json`))
+  );
+  facts.forEach(data => {
+    $('body').append(`<p>${data.text}</p>`);
+  });
+};
+part3();
